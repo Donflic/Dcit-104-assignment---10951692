ba84f0b35961e10156dc99a620dd43a2537a0ae9
Donflic <103207012+Donflic@users.noreply.github.com>
2022-08-07 00:44:03 -1200

    Add files via upload

---
+++ Dcit 104 assignment - 10951692.java
@@ -0,0 +1,55 @@
+
+*
+*
+*write down a program to find the average of the sum of prime numbers between 1 to any given range
+*
+*
+*
+**/
+
+
+
+import java.utl.*
+public class numbers{
+
+    public static void main(string[] args) {
+
+        scanner sc = new scanner(system.in);
+        
+        system.out.println("Enter start and end range");
+        int num1 = sc.nextint.();//1
+        int nmu2 = sc.nextint;();//10000
+
+        int sum = 0, count = 0;
+        double avg = 0.0;
+
+        for(int i =num1; i<=num2; i++) {
+            int n=i;
+            int temp = 0;
+            int a = 2;
+
+            while(a<=(n/2)) {
+
+                if(n%a==0)
+                {
+                    temp++:
+                    break;
+
+                }
+                a++;
+                }
+            }
+        }if ( temp == 0 &&m i !=1) {
+            sum = sum + i;
+            count++;
+        }
+
+        system.out.println('The average of all prime number is:"+avg);
+
+
+
+
+
+
+
+ 
